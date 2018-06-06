d69f886dda34fecdc96705246cf88089bcf658a9
Mike Cluck <mcluck90@gmail.com>
2018-06-06 13:57:45 -0400

    Close button closes the window

--- src/App/Header.component.jsx
+++ src/App/Header.component.jsx
@@ -1,9 +1,14 @@
+import { remote } from 'electron';
 import * as React from 'react';
 import * as styles from './Header.css';
+
+const onClose = () => {
+	remote.getCurrentWindow().close();
+};
 
 export const Header = () => (
 	<div className={styles.header}>
 		<div className={styles.filler} />
-		<div className={styles.closeButton}>x</div>
+		<div className={styles.closeButton} onClick={onClose}>x</div>
 	</div>
 );
